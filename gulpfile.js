4407628af6868b8b0d3a47eaa41a6acf09624efa
Yanis Lukes <pendrokar@gmail.com>
2019-08-13 17:12:12 +0300

    gulp: added sass compilation

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,35 @@
-var gulp = require("gulp");
-var browserify = require("browserify");
-var source = require('vinyl-source-stream');
-var ts = require('gulp-typescript');
-var tsify = require("tsify");
-var paths = {
-    pages: ['index.html']
-};
+const gulp = require("gulp");
+// const browserify = require("browserify");
+// const source = require('vinyl-source-stream');
+const ts = require('gulp-typescript');
+// const tsify = require("tsify");
+const sass = require('gulp-sass');
+//const paths = {
+//    pages: ['index.html']
+//};
 
-gulp.task('default', function () {
+// SCSS => CSS
+sass.compiler = require('node-sass');
+
+const sassTask = function () {
+    return gulp.src('./sass/**/*.scss')
+        .pipe(sass({
+            sourceMap: false,
+            outputStyle: 'compressed'
+        }).on('error', sass.logError))
+        .pipe(gulp.dest('./css'));
+}
+
+gulp.task('sass', sassTask);
+
+gulp.task('sass:watch', function () {
+    gulp.watch('./sass/**/*.scss', ['sass']);
+});
+
+
+
+// TypeScript => JavaScript
+const tsTask = function () {
     return gulp.src('js/*.ts')
         .pipe(ts({
             target: 'es6',
@@ -15,16 +37,22 @@ gulp.task('default', function () {
             noImplicitAny: true
         }))
         .pipe(gulp.dest('js'));
-});
+}
+gulp.task('tsc', tsTask);
+
+
+
+// Run default tasks
+gulp.task('default', gulp.series(sassTask, tsTask));
 
 // var tsProject = ts.createProject("tsconfig.json");
+
+/*
 
 gulp.task("copy-html", function () {
     return gulp.src(paths.pages)
         .pipe(gulp.dest("dist"));
 });
-
-/*
 gulp.task("default", gulp.series("copy-html", function () {
     return browserify({
         basedir: '.',
